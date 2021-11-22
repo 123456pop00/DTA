84976ee5cc1ba24e89c9be21f97282d9dfddfc50
hoadz <nguyenvieth0a1998@gmail.com>
2021-11-23 00:02:03 +0700

    them goi service

--- src/main.js
+++ src/main.js
@@ -5,8 +5,26 @@ import vuetify from './plugins/vuetify'
 import './plugins'
 import store from './store'
 import { sync } from 'vuex-router-sync'
-
+// import plugin
+import { TiptapVuetifyPlugin } from 'tiptap-vuetify'
+// don't forget to import CSS styles
+import 'tiptap-vuetify/dist/main.css'
+// Vuetify's CSS styles 
+import 'vuetify/dist/vuetify.min.css'
+// icons. Only if you need mdi or fortawesome icons
+import "@mdi/font/css/materialdesignicons.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 Vue.config.productionTip = false
+Vue.use(TiptapVuetifyPlugin, {
+  // the next line is important! You need to provide the Vuetify Object to this place.
+  vuetify, // same as "vuetify: vuetify"
+  // optional, default to 'md' (default vuetify icons before v2.0.0)
+  iconsGroup: 'sm'
+})
+// Vue.use(TiptapVuetifyPlugin, {
+//   // "md" (default), "fa", "mdi"
+//   // iconsGroup // same as "iconsGroup: iconsGroup"
+// });
 
 sync(store, router)
 
--- src/views/TextEditor.vue
+++ src/views/TextEditor.vue
@@ -1,72 +1,70 @@
 <template>
   <div>
-    <h2 class="mb-3">
-      The
-      <code>placeholder</code> prop
-    </h2>
-    <v-btn
-      href="#"
-      color="orange"
-      @click="onUpdate"
-    > get result
-    </v-btn>
-    <tiptap-vuetify v-model="content" :extensions="extensions" placeholder="Write something …"
-      output-format="html"
-      @update="onUpdate"
-    />
-    <div class="tiptap-vuetify-editor__content" v-html="content"/>
+    <h2 class="mb-3">The</h2>
+    <v-btn href="#" color="orange"> get result </v-btn>
+    <!-- <input type="text" id="abc"> -->
+    <tiptap-vuetify v-model="content" :extensions="extensions" />
+    <div class="tiptap-vuetify-editor__content" v-html="content" ></div>
+    <button  v-on:click="sendData12"> Send Data</button>
   </div>
 </template>
 
 <script>
-  import {
-    TiptapVuetify,
-    Bold,
-    Italic,
-    Strike,
-    Underline,
-    Code,
-    Paragraph,
-    BulletList,
-    OrderedList,
-    ListItem,
-    Link,
-    Blockquote,
-    HardBreak,
-    HorizontalRule,
-    History,
-    Image,
-  } from 'tiptap-vuetify'
-
-  export default {
-    name: 'TextEditorView',
-    // specify TiptapVuetify component in "components"
-    components: { TiptapVuetify },
-    data: () => ({
-      extensions: [
-        History,
-        Image,
-        Blockquote,
-        Link,
-        Underline,
-        Strike,
-        Italic,
-        ListItem,
-        BulletList,
-        OrderedList,
-        Bold,
-        Link,
-        Code,
-        HorizontalRule,
-        Paragraph,
-        HardBreak,
-      ],
-      content: '',
-    }),
-    methods: {
-      onUpdate () {
-        console.log(this.content)
-      },
+import {
+  TiptapVuetify,
+  Bold,
+  Italic,
+  Strike,
+  Underline,
+  Code,
+  Paragraph,
+  BulletList,
+  OrderedList,
+  ListItem,
+  Link,
+  Blockquote,
+  HardBreak,
+  HorizontalRule,
+  History,
+  Image,
+} from "tiptap-vuetify";
+import axios from "axios";
+export default {
+  name: "TextEditorView",
+  // specify TiptapVuetify component in "components"
+  components: { TiptapVuetify },
+  data: () => ({
+    extensions: [
+      History,
+      Image,
+      Blockquote,
+      Link,
+      Underline,
+      Strike,
+      Italic,
+      ListItem,
+      BulletList,
+      OrderedList,
+      Bold,
+      Link,
+      Code,
+      HorizontalRule,
+      Paragraph,
+      HardBreak,
+    ],
+    content: "",
+  }),
+  methods: {
+    sendData12: function (event) {
+      console.log("senddata");
+      // axios
+      //   .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      //   .then((response) => console.log(response));
+      // thay thanh post va truyen this content vào
     },
-  }
+    onUpdate() {
+      console.log(this.content);
+    },
+  },
+};
 </script>
